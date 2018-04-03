27efa965248fdf18eecbe5d67bb9befe6440c76e
Jim Bevington <jim.bevington@hotmail.com>
2018-04-03 16:31:28 +0100

    added requires and setup Dinos in jurassicPark_spec

---
+++ specs/jurassicPark_spec.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var Dinosaur = require('../Dinosaur.js');
+var Enclosure = require('../Enclosure.js');
+
+describe('Jurassic Park', function(){
+
+  beforeEach(function(){
+    brachio = new Dinosaur('Brachiosaurus', 2);
+    steg = new Dinosaur('Stegosaurus', 4);
+    compy = new Dinosaur('Compsognathus', 10);
+  })
+
+
+
+})
